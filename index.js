097ea85030cd791137e4d53a3d7183d33811041c
Emmanuel-hub-bit <emmanuelonyango467@gmail.com>
2024-06-30 18:03:29 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+{
+    var customerName = 'bob';
+}
+
+function upperCaseCustomerName() {
+    if (window.customerName) {
+      window.customerName = window.customerName.toUpperCase();
+    }
+  }
+
+  function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer(newCustomer) {
+    window.bestCustomer = newCustomer;
+  }
+
+  const leastFavoriteCustomer = 'Alice';
+
+  // Function that attempts to change the value of the constant
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Bob'; // Attempting to reassign the constant variable
+  }
